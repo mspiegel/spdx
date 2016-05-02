1628cc2ecf661257c47d68b991d04ca98ac6abd1
Michael Spiegel <michael.spiegel@capitalone.com>
2016-05-02 15:11:29 -0400

    Move compose functions to another file.

---
+++ compose.go
@@ -0,0 +1,20 @@
+package spdx
+
+type strFunc func(s string) string
+
+func compose2(a strFunc, b strFunc) strFunc {
+	return func(s string) string {
+		return b(a(s))
+	}
+}
+
+func compose(fns ...strFunc) strFunc {
+	return func(s string) string {
+		var res strFunc
+		res = fns[0]
+		for i := 1; i < len(fns); i++ {
+			res = compose2(res, fns[i])
+		}
+		return res(s)
+	}
+}
