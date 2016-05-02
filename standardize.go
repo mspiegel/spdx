55154bd010f6bad74e94d4c9c7470c0d2bb22531
Michael Spiegel <michael.spiegel@capitalone.com>
2016-05-02 13:12:34 -0400

    Additional SPDX matching guidelines.
    Implement 5.1.2 and 8.1.1.

--- standardize.go
+++ standardize.go
@@ -10,24 +10,97 @@ import (
 
 var multispace = regexp.MustCompile(`[[:space:]]+`)
 
-// QUOTATION MARK, APOSTROPHE
-// LEFT SINGLE QUOTATION MARK, RIGHT SINGLE QUOTATION MARK
-// LEFT DOUBLE QUOTATION MARK, RIGHT DOUBLE QUOTATION MARK
+// quotation mark
+// apostrophe
+// left single quotation mark
+// right single quotation mark
+// left double quotation mark
+// right double quotation mark
 var anyquote = regexp.MustCompile("[\u0022\u0027\u2018\u2019\u201c\u201d]")
 
+// hyphen-minus
+// hyphen
+// non-breaking hyphen
+// figure dash
+// en dash
+// em dash
+// horizontal bar
+var anydash = regexp.MustCompile("[\u002d\u2010-\u2015]")
+
+var varietals = map[string] string {
+"acknowledgment" : "acknowledgement",
+"analogue" : "analog",
+"analyse" : "analyze",
+"artefact" : "artifact",
+"authorisation" : "authorization",
+"authorised" : "authorized",
+"calibre" : "caliber",
+"cancelled" : "canceled",
+"capitalisations" : "capitalizations",
+"catalogue" : "catalog",
+"categorise" : "categorize",
+"centre" : "center",
+"emphasised" : "emphasized",
+"favour" : "favor",
+"favourite" : "favorite",
+"fulfil" : "fulfill",
+"fulfilment" : "fulfillment",
+"initialise" : "initialize",
+"judgment" : "judgement",
+"labelling" : "labeling",
+"labour" : "labor",
+"licence" : "license",
+"maximise" : "maximize",
+"modelled" : "modeled",
+"modelling" : "modeling",
+"offence" : "offense",
+"optimise" : "optimize",
+"organisation" : "organization",
+"organise" : "organize",
+"practise" : "practice",
+"programme" : "program",
+"realise" : "realize",
+"recognise" : "recognize",
+"signalling" : "signaling",
+"sub-license" : "sublicense",
+"sub license" : "sublicense",
+"utilisation" : "utilization",
+"whilst" : "while",
+"wilful" : "wilfull",
+"non-commercial" : "noncommercial",
+"per cent" : "percent",
+"copyright owner" : "copyright holder",
+}
+
 // 3.1.1 Guideline: All whitespace should be treated as a single blank space.
-func removeMultispace(input string) string {
-  return multispace.ReplaceAllString(input, " ")
+func removeMultispace(s string) string {
+  return multispace.ReplaceAllString(s, " ")
 }
 
 // 4.1.1 Guideline: All upper case and lower case letters should be
 // treated as lower case letters.
-func removeUppercase(input string) string {
-  return strings.ToLower(input)
+func removeUppercase(s string) string {
+  return strings.ToLower(s)
 }
 
 // 5.1.3 Guideline: Quotes. Any variation of quotations (single, double,
 // curly, etc.) should be considered equivalent.
-func standardQuotes(input string) string {
-  return anyquote.ReplaceAllString(input, `'`)
+func standardQuotes(s string) string {
+  return anyquote.ReplaceAllString(s, `'`)
+}
+
+// 5.1.2 Guideline: Hyphens, Dashes.  Any hyphen, dash, en dash, em dash,
+// or other variation should be considered equivalent.
+func standardDashes(s string) string {
+  return anydash.ReplaceAllString(s, `-`)
+}
+
+// 8.1.1 Guideline: The words in the following columns are considered
+// equivalent and interchangeable.
+// Precondition: input is all lowercase characters
+func standardVarietals(s string) string {
+  for k, v := range varietals {
+    s = strings.Replace(s, k, v, -1)
+  }
+  return s
 }
--- standardize_test.go
+++ standardize_test.go
@@ -26,3 +26,21 @@ func TestStandardQuotes(t *testing.T) {
   assert.Equal(t, "'", standardQuotes("“"))
   assert.Equal(t, "'", standardQuotes("”"))
 }
+
+func TestStandardDashes(t *testing.T) {
+  assert.Equal(t, "", standardDashes(""))
+  assert.Equal(t, "-", standardDashes("-")) // hyphen-minus
+  assert.Equal(t, "-", standardDashes("\u2010")) // hyphen
+  assert.Equal(t, "-", standardDashes("\u2011")) // non-breaking hyphen
+  assert.Equal(t, "-", standardDashes("\u2012")) // figure dash
+  assert.Equal(t, "-", standardDashes("\u2013")) // en dash
+  assert.Equal(t, "-", standardDashes("\u2014")) // em dash
+  assert.Equal(t, "-", standardDashes("\u2015")) // em dash
+}
+
+func TestStandardVarietals(t *testing.T) {
+  assert.Equal(t, "", standardVarietals(""))
+  assert.Equal(t, "center", standardVarietals("centre"))
+  assert.Equal(t, "center center", standardVarietals("centre centre"))
+  assert.Equal(t, "center offense", standardVarietals("centre offence"))
+}
